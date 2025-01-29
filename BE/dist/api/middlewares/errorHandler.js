1e1d711684c0befcec32fc1dcfc3304edcb633df
hong159 <2121051115@student.humg.edu.vn>
2025-01-29 19:41:37 +0700

    Remove node_modules from Git cache

--- BE/dist/api/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorHandle = void 0;
-const enums_1 = require("../enums");
-const ErrorHandle = (err, req, res) => {
-    console.error(err.stack);
-    res.status(enums_1.ResCode.INTERNAL_SERVER_ERROR).json({ error: "Something went wrong!" });
-};
-exports.ErrorHandle = ErrorHandle;
