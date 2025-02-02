4944050e4bc2b8a0a56efec8d36c684f709fc1e5
hong159 <2121051115@student.humg.edu.vn>
2025-02-02 11:11:54 +0700

    Add initial frontend structure with sign-in page and input component

---
+++ FE/src/components/input-components/input-with-border.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const InputComponent=({type,label }:{type: string, label: string})=>{
+    const name = label.toLowerCase().replace(' ', '_');
+    const id = label.replace(' ', '_');
+    return(
+        <div className={`col-span-6 `}>
+    <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+        {label}
+        </label>
+
+        <input
+    type={type}
+    id={id}
+    name={name}
+    className="mt-1 p-2 w-full  rounded-md border-2 border-gray-200 bg-white text-sm text-gray-700 shadow-xs"
+        />
+        </div>)
+}
